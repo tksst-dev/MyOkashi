15f24d3a82feaa23d91c1f3e229e30cb5f7ccff5
tksst-air <tksst51@gmail.com>
2022-07-10 17:13:40 +0900

    2022.7.10 サンプルプログラム完成

--- MyOkashi/ContentView.swift
+++ MyOkashi/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @StateObject var okashiDataList = OkashiData()
     // 入力された文字列を保持する状態変数
     @State var inputText = ""
+    // SafariViewの表示有無を管理する変数
+    @State var showSafari = false
     
     var body: some View {
         // 垂直にレイアウト（縦方向にレイアウト）
@@ -25,6 +27,44 @@ struct ContentView: View {
                 }
                 .submitLabel(.search)
                 .padding()
+            
+            // リスト表示する
+            List(okashiDataList.okashiList) { okashi in
+                // １つ１つの要素が取り出される
+                
+                // ボタンを用意する
+                Button(action: {
+                    // SafariViewを表示する
+                    showSafari.toggle()
+                }) {
+                    // Listの表示内容を生成する
+                    // 水平にレイアウト（横方向にレイアウト）
+                    HStack {
+                        // 画像を読み込み、表示する
+                        AsyncImage(url: okashi.image) { image in
+                            // 画像を表示する
+                            image
+                                // リサイズ
+                                .resizable()
+                                // アスペクト比（縦横比）を維持してエリア内に収まるようにする
+                                .aspectRatio(contentMode: .fit)
+                                // 高さ40
+                                .frame(height: 40)
+                        } placeholder: {
+                            // 読み込み中はインジケータを表示する
+                            ProgressView()
+                        }
+                        // テキスト表示する
+                        Text(okashi.name)
+                    } // HStack終了
+                } // Button終了
+                .sheet(isPresented: self.$showSafari, content: {
+                    // SafariViewを表示する
+                    SafariView(url: okashi.link)
+                        // 画面下部がセーフエリア外までいっぱいになるように指定
+                        .edgesIgnoringSafeArea(.bottom)
+                }) // sheet終了
+            } // List終了
         }
     }
 }
--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -6,13 +6,88 @@
 //
 
 import Foundation
+import UIKit
 
+// Identifialeプロトコルを利用して、お菓子の情報をまとめる構造体
+struct OkashiItem : Identifiable {
+    let id = UUID()
+    let name: String
+    let link: URL
+    let image: URL
+}
 // お菓子データ検索用クラス
 class OkashiData: ObservableObject {
+    // JSONのデータ構造
+    struct ResultJson: Codable {
+        // JSONのitem内のデータ構造
+        struct Item: Codable {
+            // お菓子の名称
+            let name: String?
+            // 掲載URL
+            let url: URL?
+            // 画像URL
+            let image: URL?
+        }
+        
+        // 複数要素
+        let item: [Item]?
+    }
+    
+    // お菓子のリクエスト（Identifiableプロトコル）
+    @Published var okashiList: [OkashiItem] = []
     
     // Web API検索用メソッド　第一引数：keyword 検索したいワード
     func searchOkashi(keyword: String) async {
         // デバックエリアに出力
         print(keyword)
+        
+        // お菓子の検索キーワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURLの組み立て
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {return}
+
+        // デバックエリアに出力
+        print(req_url)
+        
+        do {
+            // リクエストURLからダウンロード
+            let (data, _) = try await URLSession.shared.data(from: req_url)
+            
+            // JSONDecoderのインスタンス取得
+            let decoder = JSONDecoder()
+            // 受け取ったJSONデータをパース（解析）して格納
+            let json = try decoder.decode(ResultJson.self, from: data)
+            
+            //print(json)
+            
+            // お菓子の情報が取得できているか確認
+            guard let items = json.item else { return }
+            // @Publishedの変数を更新するときはメインスレッドで更新する必要がある
+            DispatchQueue.main.async {
+                self.okashiList.removeAll()
+            }
+            // 取得しているお菓子の数だけ処理
+            for item in items {
+                // お菓子の名称、掲載URL、画像URLをアンラップ
+                if let name = item.name,
+                    let link = item.url,
+                    let image = item.image {
+                    // １つのお菓子を構造体でまとめて管理
+                    let okashi = OkashiItem(name: name, link: link, image: image)
+                    // @Publishedの変数を更新するときはメインスレッドで更新する必要がある
+                    DispatchQueue.main.async {
+                        // お菓子の配列へ追加
+                        self.okashiList.append(okashi)
+                    }
+                }
+            }
+            print(self.okashiList)
+        } catch {
+            // エラー処理
+            print("エラーが出ました")
+        }
     }
 }
